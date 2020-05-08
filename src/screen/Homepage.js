5828069917f6d8f3fdf320c86489c4a6f3fb7409
anuradhabharti <anuraadha.bharti@mountblue.tech>
2020-05-08 14:21:37 +0530

    add screen

---
+++ src/screen/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../components/Header'
+import CoinList from '..components/CoinList'
+function HomePage() {
+        return (
+           <section>
+               <Header/>
+               <CoinList/>
+           </section>
+          )
+}
+ 
+export default HomePage;
