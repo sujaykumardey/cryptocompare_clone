8d7b96f991e66f18aa6cf7d47a53cc2f700d9252
sujay dey <sujay.dey@mountblue.tech>
2020-05-13 23:55:45 +0530

    add navbar footer component add modal pop up in login/signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/footer'
 
 export default class App extends Component {
   render() {
     return (
+      <>
+      <Navbar />
       <div> TEAM DENVER </div>
+      <Footer/>
+    </>
     );
   }
 }
