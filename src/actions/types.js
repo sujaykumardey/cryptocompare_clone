11c1b750ff1d2cb157ee32f6174e0812c5897dbe
anuradhabharti <anuraadha.bharti@mountblue.tech>
2020-05-21 13:29:22 +0530

    remove conflict in json package

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 export const FETCH_CRYPTO= 'FETCH_CRYPTO';
 export const FETCH_ALLCRYPTO= 'FETCH_ALLCRYPTO';
 export const FETCH_FORUM= 'FETCH_FORUM';
 export const FETCH_NEWS= 'FETCH_NEWS';
-
-=======
 export const FETCH_HISTORICAL_DATA_DAY = 'FETCH_HISTORICAL_DATA_DAY';
->>>>>>> coin-details-clone
 
