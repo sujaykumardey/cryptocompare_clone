3107b26f45cd7c7ef6434cd4b46d0775d4b524d2
sujay dey <sujay.dey@mountblue.tech>
2020-05-21 23:15:45 +0530

    add some changes in navbar

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,27 +1,19 @@
-// import * as actions from './types'
-
-
-
-// export const fetchHistoricalDayData = () => (dispatch) => {
-//     let url = `https://api.trello.com/1/members/me/boards?key=${key}&token=${token}`
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(boards => {
-//         dispatch({
-//             type:actions.FETCH_BOARDS,
-//             payload: boards
-//         })
-//       })
-//       .catch( e => console.log("Error in fetching boards"))
-    
-// }
-
-// // 
-import {FETCH_USER,POST_COIN,DELETE_COIN,DELETE_USER} from './types';
-import {registrationUser,loginUser,addCoin,deleteCoin} from '../assets/credentials';
+import {
+  FETCH_USER,
+  POST_COIN,
+  DELETE_COIN,
+  DELETE_USER,
+  UPDATE_COIN_PRICE,
+} from './types';
+import {
+  registrationUser,
+  loginUser,
+  addCoin,
+  deleteCoin,
+} from '../assets/credentials';
 
 export const userResistration = (data) => (dispatch) => {
-  registrationUser(data).then((user) => {  
+  registrationUser(data).then((user) => {
     dispatch({
       type: FETCH_USER,
       payload: user,
@@ -29,8 +21,8 @@ export const userResistration = (data) => (dispatch) => {
   });
 };
 
-export const userLogin= (data) => (dispatch) => {
-  loginUser(data).then((user) => {  
+export const userLogin = (data) => (dispatch) => {
+  loginUser(data).then((user) => {
     dispatch({
       type: FETCH_USER,
       payload: user,
@@ -38,29 +30,36 @@ export const userLogin= (data) => (dispatch) => {
   });
 };
 
-
-export const addCoinWallet= (data,token) => (dispatch) => {
-  console.log(data,token);
-  addCoin(data,token).then((coin) => {  
-    dispatch({      
+export const addCoinWallet = (data, token) => (dispatch) => {
+  console.log(data, token);
+  addCoin(data, token).then((coin) => {
+    dispatch({
       type: POST_COIN,
       payload: coin,
     });
   });
 };
 
-export const onDeleteCoin= (id,token) => (dispatch) => {
-    deleteCoin(id,token).then((coin) => {  
-      dispatch({      
+export const onDeleteCoin = (id, token) => (dispatch) => {
+  deleteCoin(id, token).then((coin) => {
+    dispatch({
       type: DELETE_COIN,
       payload: coin,
     });
   });
 };
 
-export const Logout= () => (dispatch) => {
-  dispatch({      
+export const Logout = () => (dispatch) => {
+  dispatch({
     type: DELETE_USER,
     payload: [],
+  });
+};
+
+export const bitCoinPrice = (price) => (dispatch) => {
+  console.log(price, 'reducers');
+  dispatch({
+    type: UPDATE_COIN_PRICE,
+    payload: price,
   });
 };
